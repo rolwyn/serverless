9fcdf7393afd0aa6e77b83c15e5831f5ea1e0c44
Rolwyn Ronald Quadras <quadras.r@northeastern.edu>
2022-04-12 00:58:58 -0400

    Added lambda function - tested

--- index.js
+++ index.js
@@ -1,3 +1,84 @@
+const awssdk = require("aws-sdk")
+awssdk.config.update({region: 'us-east-1'})
+const documentClient = new awssdk.DynamoDB.DocumentClient()
+const crypto = require('crypto')
+const ses = new awssdk.SES()
+
 exports.emailVerification = (event, context, callback) => {
-    console.log("Testing last time")
+    
+    console.log("lambda function for sending email")
+    console.log(event)
+
+    let snsmessage = event.Records[0].Sns.Message
+    console.log(Object.keys(event.Records[0].Sns))
+    console.log(event.Records[0].Sns.MessageAttributes)
+    console.log("email is")
+    console.log(event.Records[0].Sns.MessageAttributes.emailid.Value)
+    let emailId = event.Records[0].Sns.MessageAttributes.emailid.Value
+    let tokenValue = event.Records[0].Sns.MessageAttributes.token.Value
+    
+    let getEmailListParams = {
+        TableName: 'emailListTbl',
+        Key: {
+            emailid: emailId
+        }
+    }
+    
+    let putEmailParams = {
+        TableName: "emailListTbl",
+        Item: {
+            emailid: emailId
+        }
+    }
+    
+    documentClient.get(getEmailListParams, (err, emaillist) => {
+        if (err) console.log(err)
+        else {
+            console.log("email list is --------------------->//")
+            console.log(emaillist)
+            console.log("length is --------------------->//")
+            console.log(Object.keys(emaillist).length)
+            // email not found, add email to list
+            if (Object.keys(emaillist).length === 0) {
+                let emailInfoParams = {
+                    Destination: {
+                        ToAddresses: [emailId]
+                    },
+                    Message: {
+                        Body: {
+                            Text: {
+                                Data: "http://"+process.env.DomainName+"/v1/verifyUserEmail?email="+emailId+"&token="+tokenValue
+                            }
+                        },
+                        Subject: {
+                            Data: "Email Verification Request"
+                        }
+                    },
+                    Source: "noreply@"+process.env.DomainName
+                };
+                
+                console.log("reached to email")
+                ses.sendEmail(emailInfoParams, function (err, data) {
+                    if (err) {
+                        console.log(err)
+                        callback(null, {err: err})
+                    }
+                    else {
+                        console.log("Email is sent!");
+                        documentClient.put(putEmailParams, (err, data) => {
+                            if (err) {
+                                console.log("Error in adding item to Email list table")
+                            }
+                            else {
+                                console.log(`Email id ${putEmailParams.Item.emailid} added to sent list`)
+                            }
+                        })
+                        callback(null, {data: data})
+                    }
+                });
+            } else {
+                console.log("Email already sent")
+            }
+        }
+    })
 }
